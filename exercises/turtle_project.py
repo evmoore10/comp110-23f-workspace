4bb821b6d8a8ec66550e2cf448d4b342639e9ffb
Evan Moore <evmoore@ad.unc.edu>
2023-10-23 17:14:08 -0400

    k

--- exercises/turtle_project.py
+++ exercises/turtle_project.py
@@ -23,7 +23,7 @@ def main() -> None:
 
 
 def draw_track(speed: Turtle, scale_factor: float) -> None:
-    "Draws a track with scale factor."
+    """Draws a track with scale factor."""
     speed.fillcolor("sandybrown")
     speed.penup()
     speed.goto(-250 * scale_factor, 200 * scale_factor)
@@ -40,10 +40,10 @@ def draw_track(speed: Turtle, scale_factor: float) -> None:
 
 
 def background(speed: Turtle) -> None:
-    "Fills background in."
+    """Fills background in."""
     speed.fillcolor("green")
     speed.penup()
-    speed.goto(-500,-500)
+    speed.goto(-500 , -500)
     speed.pendown()
     speed.begin_fill()
     for each in range(4):
@@ -52,23 +52,23 @@ def background(speed: Turtle) -> None:
     speed.end_fill()
     
 def discus(speed: Turtle) -> None:
-    "Creates discus pit."
+    """Creates discus pit."""
 
     def net(speed: Turtle) -> None:
-        """Creates a net around the pit"""
+        """Creates a net around the pit."""
         speed.penup()
         speed.pensize(3)
         speed.pencolor("black")
-        speed.goto(200, -280)
+        speed.goto(200 , -280)
         speed.pendown()
         speed.circle(60 , 200)
         speed.pencolor("white")
 
     def foul_lines(speed: Turtle) -> None:
-        """Creates the sector lines"""
+        """Creates the sector lines."""
         speed.penup()
-        speed.pensize(1.5)
-        speed.goto(190, -248)
+        speed.width(1.5)
+        speed.goto(190 , -248)
         speed.pendown()
         speed.left(10)
         speed.forward(400)
@@ -80,7 +80,7 @@ def discus(speed: Turtle) -> None:
     
     speed.fillcolor("grey")
     speed.penup()
-    speed.goto(200, -250)
+    speed.goto(200 , -250)
     speed.begin_fill()
     speed.pendown()
     speed.circle(30)
@@ -89,12 +89,12 @@ def discus(speed: Turtle) -> None:
     foul_lines(speed)
 
 def shotput(speed: Turtle) -> None:
-    "Creates shotput pit."
+    """Creates shotput pit."""
     
     def foul_lines(speed: Turtle) -> None:
-        """Draws the sector lines"""
+        """Draws the sector lines."""
         speed.penup()
-        speed.pensize(1.5)
+        speed.width(1.5)
         speed.goto(-157 , 300)
         speed.pendown()
         speed.right(15)
@@ -109,7 +109,7 @@ def shotput(speed: Turtle) -> None:
     speed.penup()
     speed.pensize(1)
     speed.setheading(0)
-    speed.goto(-180, 290)
+    speed.goto(-180 , 290)
     speed.begin_fill()
     speed.pendown()
     speed.circle(30)
---
+++ lessons/turtle_tutorial.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, colormode, done
+leo: Turtle = Turtle()
+
+
+leo.forward(300)
+leo.left(90)
+leo.forward(300)
+leo.left(90)
+leo.forward(300)
+leo.left(90)
+leo.forward(300)
+leo.left(90)
+
+
+i: int = 0
+while (i < 4):
+    leo.forward(300)
+    leo.left(90)
+    i = i + 1
+    
+    
